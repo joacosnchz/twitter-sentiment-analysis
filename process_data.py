9ab719ea9cbb7b2c3fd1430d14335ac8bc7d3fda
Joaquin Sanchez <sanchezjoaquin1995@gmail.com>
2022-06-24 12:36:53 -0300

    Train ML model

--- process_data.py
+++ process_data.py
@@ -2,6 +2,11 @@ import os
 import html
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+nltk.download('vader_lexicon')
+sentiment = SentimentIntensityAnalyzer()
 
 spark = SparkSession.builder.appName('TwitterSentiment').getOrCreate()
 
@@ -15,6 +20,18 @@ def unescape_string(escaped):
     return html.unescape(escaped)
 unescape_udf = F.udf(unescape_string)
 
+# UDF aimed to perform sentiment analysis
+def label_data(text):
+    sentiment_dict = sentiment.polarity_scores(text)
+
+    if sentiment_dict['pos'] >= 0.05:
+        return 1
+    elif sentiment_dict['neg'] >= 0.05:
+        return -1
+    else:
+        return 0
+label_data_udf = F.udf(label_data, returnType='INTEGER')
+
 duplicates = raw_data.where(raw_data.full_text.startswith('RT') == False) \
     .dropDuplicates(['id'])
 splitted = duplicates.withColumn('words', F.explode(F.split('full_text', ' '))) \
@@ -27,12 +44,14 @@ grouped = filtered.withWatermark('timestamp', '50 seconds') \
     .groupBy('id', 'timestamp') \
     .agg(F.concat_ws(',', F.collect_list(filtered.words)).alias('full_text')) \
     .select('id', 'timestamp', F.regexp_replace('full_text', ',', ' ').alias('full_text'))
-cleaned = grouped.withColumn('full_text', unescape_udf('full_text')) \
+cleaned = grouped.select('full_text') \
+    .withColumn('full_text', unescape_udf('full_text')) \
     .withColumn('full_text', F.regexp_replace('full_text', '[^A-Za-z0-9 ]+', '')) \
-    .withColumn('full_text', F.lower('full_text'))
+    .withColumn('full_text', F.lower('full_text')) \
+    .withColumn('target', label_data_udf('full_text'))
 
 streamingQuery = cleaned.writeStream \
-    .format("csv") \
+    .format("parquet") \
     .outputMode("append") \
     .trigger(processingTime='55 seconds') \
     .option("checkpointLocation", os.environ['HOME'] + '/Projects/twitter-sentiment/checkpoint') \
--- train_model.py
+++ train_model.py
@@ -1,32 +1,35 @@
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
+from pyspark.ml import Pipeline
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 from pyspark.ml.feature import StringIndexer
-from pyspark.ml import Pipeline
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import pandas as pd
-
-nltk.download('vader_lexicon')
+from pyspark.ml.classification import LogisticRegression
 
 spark = SparkSession.builder.appName('TrainTwitterSentimentModel').getOrCreate()
 
-schema = 'id LONG, timestamp TIMESTAMP, full_text STRING'
-df = spark.read \
-    .schema(schema) \
-    .option('header', 'false') \
-    .csv('processed')
+df = spark.read.parquet('processed')
 
-sid_obj = SentimentIntensityAnalyzer()
-def label_data(text):
-    sentiment_dict = sid_obj.polarity_scores(text)
+(train_set, test_set) = df.randomSplit([0.8, 0.2], seed = 42)
 
-    if sentiment_dict['pos'] >= 0.05:
-        return 1
-    elif sentiment_dict['neg'] >= 0.05:
-        return -1
-    else:
-        return 0
+# Converting text to an array of words
+tokenizer = Tokenizer(inputCol="full_text", outputCol="words")
 
-label_data_udf = F.udf(label_data, returnType='INTEGER')
-df.select('full_text', label_data_udf('full_text').alias('label')).show()
+# Converting words to numerical format using TF-IDF strategy
+hashtf = HashingTF(numFeatures=2**16, inputCol="words", outputCol='tf')
+idf = IDF(inputCol='tf', outputCol="features", minDocFreq=5)
+
+# Converting targets to index format
+label_stringIdx = StringIndexer(inputCol = "target", outputCol = "label")
+
+# Training the model
+lr = LogisticRegression(maxIter=100)
+pipeline = Pipeline(stages=[tokenizer, hashtf, idf, label_stringIdx, lr])
+model = pipeline.fit(train_set)
+predictions = model.transform(test_set)
+
+predictions.show()
+accuracy = predictions.filter(predictions.label == predictions.prediction).count() / float(test_set.count())
+print('Accuracy: %.2f' % accuracy)
+
+spark.stop()
+
